643020342dd6ab76f0803ada6662e1769d54fb2a
mmucahitOt <m.mucahit0@gmail.com>
2025-08-01 19:46:48 +0300

    broken code causes the deployment failed. The previous version of the app must be running on Render

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
